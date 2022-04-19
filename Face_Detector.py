83c908df3f34900bbfd8c8aa1d3e706e387c2071
BuildTools <unconfigured@null.spigotmc.org>
2022-04-19 16:15:04 -0400

    python file
    python file

---
+++ Face_Detector.py
@@ -0,0 +1,12 @@
+from random import randrange
+import cv2
+trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+img = cv2.imread('hardy.webp')
+grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+for (x,y,w,h) in face_coordinates:
+    cv2.rectangle(img, (x,y), (x+w,y+h), (randrange(256),randrange(256),randrange(256)), 3)
+#print(face_coordinates)
+cv2.imshow('Clever Programmer Face Detector', img)
+cv2.waitKey()
+print("Code Completed")
